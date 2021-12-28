f7685a7aa4e2b177281aefa964abfff6f2da9e42
dohung <dohuyhung12@gmail.com>
2021-12-28 15:32:43 +0700

    add api fetching

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./style.css";
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+
+async function getWeather(cityName) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=089669fece331a258cf56a3771f30bb9`;
+  const response = await fetch(url, { mode: "cors" });
+
+  console.log(response.json());
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getWeather(input.value);
+  form.reset();
+});
