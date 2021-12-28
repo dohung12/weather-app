021ef9c2203a5c73c22f0816cebfd2f3fb85fee7
dohung <dohuyhung12@gmail.com>
2021-12-28 19:32:35 +0700

    display current weather

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import "./style.css";
+import displayCurrent from "./displayCurrentControl";
 
 const form = document.querySelector("form");
 const input = document.querySelector("input");
+const currentWeatherContainer = document.querySelector(".current-weather");
 
 async function getWeather(cityName) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=089669fece331a258cf56a3771f30bb9`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=089669fece331a258cf56a3771f30bb9&units=metric`;
   const response = await fetch(url, { mode: "cors" });
 
-  console.log(response.json());
+  return response.json();
 }
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  getWeather(input.value);
+  getWeather(input.value).then((data) => {
+    console.log(data, typeof data);
+    const currentWeather = displayCurrent(data);
+    currentWeatherContainer.appendChild(currentWeather);
+  });
   form.reset();
 });
